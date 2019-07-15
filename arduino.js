5e5a4673e92e568ade3c98370ca4968e68607802
Fausto Fusse <faustofusse@gmail.com>
2019-07-15 18:25:58 -0300

    First commit

---
+++ arduino.js
@@ -0,0 +1,10 @@
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function () {
+    console.log('BRUH THE BOARD IS READY');
+    var led = new five.Led(12);
+    led.blink(1500);
+
+    this.repl.inject({ led });
+});
