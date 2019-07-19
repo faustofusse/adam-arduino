d97db295011476b5791119db3124a834f9eec469
FFsdvdf <43719026@ort>
2019-07-19 10:49:21 -0300

    Sensor ultrasonido

--- arduino.js
+++ arduino.js
@@ -6,5 +6,21 @@ board.on("ready", function () {
     var led = new five.Led(12);
     led.blink(1500);
 
+    var proximity = new five.Proximity({
+        controller: "HCSR04",
+        pin: 7
+    });
+
+    proximity.on("data", function() {
+        console.log("Proximity: ");
+        console.log("  cm  : ", this.cm);
+        console.log("  in  : ", this.in);
+        console.log("-----------------");
+    });
+
+    proximity.on("change", function() {
+        console.log("The obstruction has moved.");
+    });
+
     this.repl.inject({ led });
 });
